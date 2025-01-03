7692e9469e89a339d6cda23a0deead6cc6a9f02e
Arielcito <serato.arieli@gmail.com>
2025-01-03 18:04:41 -0300

    feat: change title

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import Signin from "@/components/Auth/Signin";
 export const metadata: Metadata = {
-  title: "1MomentGlobal",
-  description: "1MomentGlobal is a platform for creating and sharing moments with your friends and family.",
+  title: "1MovementGlobal",
+  description: "1MovementGlobal is a platform for creating and sharing moments with your friends and family.",
 };
 export default function Home() {
   return (
