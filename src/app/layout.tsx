26aa47f06922b4b8a1ea5bd46abf4fbd88f3e16a
Arielcito <serato.arieli@gmail.com>
2025-01-04 18:14:49 -0300

    chore: update name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import type { Metadata } from 'next'
 import { Analytics } from "@vercel/analytics/react"
 import { SpeedInsights } from "@vercel/speed-insights/next"
 export const metadata: Metadata = {
-  title: '1 Movement Global',
-  description: '1 Movement Global',
+  title: 'One Movement Global',
+  description: 'One Movement Global',
 }
 
 export default function RootLayout({
